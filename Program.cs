da680ac5e5662dd5fd3df0b263888fc7a70cc469
Guilherme domingos <gritoty001@gmail.com>
2025-04-06 16:45:11 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+        Console.Write("Digite uma frase: ");
+        string fraseOriginal = Console.ReadLine()!;
+
+      
+        string fraseCebolinha = fraseOriginal
+            .Replace('r', 'l')
+            .Replace('R', 'L');
+
+        Console.WriteLine(fraseCebolinha);
+    
+
+
